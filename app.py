403166b3d577fe9ac33991e0cbe9abdaccb113c8
Ravi Verma <44615530+raviverma9807@users.noreply.github.com>
2025-08-27 22:47:10 +0530

    update icon

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.set_page_config(
 def get_base64_of_image(image_path):
     with open(image_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode()
-img_base64 = get_base64_of_image("ravi-profile.jpg")
+img_base64 = get_base64_of_image("r.png")
 
 st.markdown(
     f"""
